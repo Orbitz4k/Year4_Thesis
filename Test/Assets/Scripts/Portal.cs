554280e3cb4bf656c7ccbd2fd9de21759474fda5
Orbitz4k <B00124102@mytudublin.ie>
2023-03-12 09:55:47 +0000

    added portal script

---
+++ Test/Assets/Scripts/Portal.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    //scene number value for teleporter, set in editor
+    public int sceneNumber;
+
+    public ParticleSystem particleEffect;
+    //when the user collides with the portal they are sent to a different level
+     void OnCollisionEnter(Collision collision)
+    {
+        //ensures that only player will trigger this
+        if (collision.gameObject.tag == "Plaer")
+        {
+            SceneManager.LoadScene(sceneNumber);
+        }
+        
+    }
+}
